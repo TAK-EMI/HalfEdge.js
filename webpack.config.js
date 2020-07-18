b57f3056b9e74b23c78eff465b59c84621c603d3
TAK_EMI <tumbling.earth@gmail.com>
2020-07-18 16:18:05 +0900

    最初のコミット

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	mode: "development",
+	entry: {
+		HalfEdge: "./src/ts/index.ts",
+	},
+	output: {
+		path: `${__dirname}/dist`,
+		filename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				enforce: 'pre',
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					fix: true
+				}
+			},
+			{
+				test: /\.ts$/,
+				loader: "ts-loader"
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	}
+};
